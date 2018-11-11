a90a5acd591a83895b6a05b8a4732ea41ee87e92
Akash Agarwal <akash.kumar@axisrooms.com>
2018-11-11 22:10:42 +0530

    Development to mongodb

--- config/env/production.js
+++ config/env/production.js
@@ -13,8 +13,8 @@ module.exports = {
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
   db: {
-    // uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
-    uri: 'mongodb://admin:Abcd123$01@ds237770.mlab.com:37770/avleisure',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    // uri: 'mongodb://admin:Abcd123$01@ds237770.mlab.com:37770/avleisure',
     options: {
       /**
       * Uncomment to enable ssl certificate based authentication to mongodb
